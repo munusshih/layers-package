d8d569916691cbb18103e75c973cf84e73606877
Munus Shih <munusshih@newschool.edu>
2021-11-02 21:41:42 -0400

    Add files via upload

--- left.js
+++ left.js
@@ -1,8 +1,12 @@
 function leftBox_draw() {
 	leftBox.push()
 
-	let ofX = 0
-	let ofY = 0
+	leftBox.translate(leftBox.width / 2, leftBox.height / 2)
+	leftBox.rotate(PI / 2)
+	leftBox.translate(-leftBox.width / 2, -leftBox.height / 2)
+
+	let ofX = (leftBox.width - leftBox.height) / 2
+	let ofY = (leftBox.height - leftBox.width) / 2
 
 	// 	background
 	leftBox.background(bgColor)
@@ -17,88 +21,53 @@ function leftBox_draw() {
 	// 	starting point
 	// leftBox.circle(ofX, ofY, 20)
 
-	// 	pattern
-
+// 	pattern
+	
+	
 	let sum = 0
 	let leftBoxPadding = 10
-	let lineWidthRatio = 0.5 * (leftBox.width - leftBoxPadding * 2)
-
-	leftBox.push()
-	leftBox.translate(-(leftBox.width / 2 - leftBoxPadding), 0)
-
+	let lineWidthRatio = 0 * (leftBox.height - leftBoxPadding * 2)
 	leftBox.strokeWeight(1)
 	leftBox.stroke(255)
-	leftBox.line(ofX + leftBoxPadding + lineWidthRatio, ofY + leftBoxPadding, ofX + leftBox.width - leftBoxPadding, ofY + leftBoxPadding)
-	leftBox.line(ofX + leftBoxPadding + lineWidthRatio, ofY + leftBoxPadding+14, ofX + leftBox.width - leftBoxPadding, ofY + leftBoxPadding+14)
+	leftBox.line(ofX + leftBoxPadding + lineWidthRatio, ofY + leftBoxPadding, ofX + leftBox.height - leftBoxPadding, ofY + leftBoxPadding)
 
+	
 	leftBox.fill(255)
-	leftBox.textFont(display)
 	leftBox.noStroke()
-	leftBox.textSize(6)
-	leftBox.textAlign(LEFT, CENTER)
-
-	leftBox.textFont("ivypresto-display")
-	let firstName = name.split(' ');
-	leftBox.text(firstName[0] + "'s ingredients  (%)", ofX + leftBox.width / 2 + 5,
-		ofY + 17.5)
+	leftBox.textSize(7)
+	leftBox.textAlign(RIGHT, CENTER)
+	leftBox.text(name+ "'s ingredients  (%)", ofX + leftBox.height - leftBoxPadding - 5,
+		ofY + 20)
 
 	for (let i = 0; i < ingred.length; i++) {
-		let lineDist = (leftBox.height - leftBoxPadding * 4) * i / ingred.length + leftBoxPadding * 2
+		let lineDist = (leftBox.width - leftBoxPadding * 4) * i / ingred.length + leftBoxPadding * 2
 		leftBox.stroke(255)
-
+		leftBox.line(ofX + leftBoxPadding + lineWidthRatio, ofY + leftBoxPadding + lineDist,
+			ofX + leftBox.height - leftBoxPadding, ofY + leftBoxPadding + lineDist)
 		leftBox.fill(layerColor[i])
 		leftBox.noStroke()
-		leftBox.circle(ofX + leftBox.width - leftBoxPadding - 7,
-			ofY + lineDist + 14, 5)
+		leftBox.circle(ofX + leftBox.height - leftBoxPadding - 7,
+			ofY + lineDist + 18, 5)
 		leftBox.fill(255)
 		leftBox.noStroke()
-		leftBox.textSize(5)
-
+		leftBox.textSize(4)
 		leftBox.textAlign(RIGHT, CENTER)
-		leftBox.text(ingred[i], ofX + leftBox.width - leftBoxPadding - 15,
-			ofY + lineDist + 13)
+		leftBox.text(ingred[i], ofX + leftBox.height - leftBoxPadding - 15,
+			ofY + lineDist + 18)
 		leftBox.textAlign(LEFT, CENTER)
-		leftBox.textSize(5.5)
-
-		leftBox.text((data[i] * 100).toFixed(2) + ' %', ofX + leftBox.width / 2 + 5,
-			ofY + lineDist + 13)
+		leftBox.textSize(6)
+		leftBox.text(int(data[i] * 100) + ' %', ofX + leftBoxPadding + 5,
+			ofY + lineDist + 18)
 	}
 
 	leftBox.stroke(255)
 	leftBox.line(ofX + leftBoxPadding + lineWidthRatio, ofY + leftBoxPadding,
-		ofX + leftBoxPadding + lineWidthRatio, ofY + (leftBox.height - leftBoxPadding))
-	leftBox.line(ofX + leftBox.width - leftBoxPadding, ofY + leftBoxPadding,
-		ofX + leftBox.width - leftBoxPadding, ofY + (leftBox.height - leftBoxPadding))
+		ofX + leftBoxPadding + lineWidthRatio, ofY + (leftBox.width - leftBoxPadding))
+	leftBox.line(ofX + leftBox.height - leftBoxPadding, ofY + leftBoxPadding,
+		ofX + leftBox.height - leftBoxPadding, ofY + (leftBox.width - leftBoxPadding))
 
 	leftBox.stroke(255)
-	leftBox.line(ofX + leftBoxPadding + lineWidthRatio, ofY + leftBox.height - leftBoxPadding, ofX + leftBox.width - leftBoxPadding, ofY + leftBox.height - leftBoxPadding)
+	leftBox.line(ofX + leftBoxPadding + lineWidthRatio, ofY + leftBox.width - leftBoxPadding, ofX + leftBox.height - leftBoxPadding, ofY + leftBox.width - leftBoxPadding)
 
-	leftBox.textFont(display)
-	let explaination = " \n\n\
-We extracted the best minerals in different soil layers and put\
-them into your formula. The pattern of each box is generated from\
-your own personal customized formula, with the width of each layer\
-visulizing the proportion of each organic ingredient in the product.\
-Every box is unique and precious, just like you."
-	
-	leftBox.stroke(255)
-	leftBox.strokeWeight(0.4)
-	leftBox.line(ofX + leftBox.width, leftBox.height - 65.5,  ofX + leftBox.width + 80, leftBox.height - 65.5)
-	
-	leftBox.fill(255)
-	leftBox.textSize(4.5)
-	leftBox.noStroke()
-	leftBox.textAlign(LEFT, CENTER)
-	leftBox.textWrap(WORD)
-
-	leftBox.textFont("ivypresto-display")
-	leftBox.text('What is under the layers of your skin?', ofX + leftBox.width,
-	leftBox.height - 70, 80)
-	
-	leftBox.textFont(display)
-	leftBox.text(explaination, ofX + leftBox.width,
-			leftBox.height - 70, 80)
-	
-	leftBox.pop()
 	leftBox.pop()
 }
